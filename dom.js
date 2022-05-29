2adcf3edffbcbf6b467a1d6de7dc68faa212fed6
sheebanmd <91521103+sheebanmd@users.noreply.github.com>
2022-05-29 18:09:02 +0530

    Done with the assigment

---
+++ dom.js
@@ -0,0 +1,29 @@
+// //console.dir(document);
+
+// // console.log(document.domain);
+
+// console.log(document.URL);
+
+// console.log(document.doctype);
+// // console.log(document.head);
+// // console.log(document.body);
+
+// console.log(document.all[10]);
+
+// document.all[10].textContent = "NEXT"
+// console.log(document.forms);
+
+console.log(document.title);
+
+var MainTile = document.getElementById('header-title');
+var Header = document.getElementById('main-header');
+// console.log(MainTile);
+// MainTile.textContent = "Item Not Listener";
+
+// MainTile.innerText = "Not Listener";
+
+Header.style.borderBottom = 'solid 3px black';
+
+var title = document.getElementById('items');
+title.style.fontWeight = 'bold';
+title.style.color = 'green';
