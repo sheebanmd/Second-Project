f288f227c694c6235f45e1d820117fe793ae3f63
sheebanmd <91521103+sheebanmd@users.noreply.github.com>
2022-05-29 19:23:25 +0530

    Targeting elements by tag name

--- dom.js
+++ dom.js
@@ -28,13 +28,19 @@
 // title.style.fontWeight = 'bold';
 // title.style.color = 'green';
 
-const item = document.getElementsByClassName('list-group-item');
-for(let i = 0; i < item.length; i++ ){
-    item[i].style.fontWeight = 'bold';
-  }
-item[2].style.backgroundColor = 'green';
+//const item = document.getElementsByClassName('list-group-item');
 
+// for(let i = 0; i < item.length; i++ ){
+//     item[i].style.fontWeight = 'bold';
+//   }
+// item[2].style.backgroundColor = 'green';
 
+const li = document.getElementsByTagName('li');
+for(let i = 0; i < li.length; i++ ){
+    li[i].style.fontWeight = 'bold';
+    li[i].style.backgroundColor = 'yellow';
+
+    }
 
 
 
